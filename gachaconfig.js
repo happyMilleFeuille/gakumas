5e85035132501fe8af971c52d0e0a965f8226ed6
happyMilleFeuille <chyoai@gmail.com>
2026-01-28 06:58:56 +0900

    3343

--- gachaconfig.js
+++ gachaconfig.js
@@ -27,15 +27,21 @@ export const CURRENT_PICKUPS = {
     test: { 
         // 픽업 연출(블랙아웃) 확인용 카드 리스트
         pssr: [
-            { id: 'ssrtemari_campusfes', char: 'temari' },            
+            { id: 'ssrtemari_campusfes', char: 'temari' },   
+            { id: 'ssrchina_michinaruunit', char: 'china' }, 
+            { id: 'ssrhiro_michinaruunit', char: 'hiro' },                                  
         ],
         // 일반 SSR 연출 확인용 카드 리스트 (블랙아웃 안 터짐)
         others: [
             'ssrrinami_1st2another',
             'ssrume_starmineunit',
             'ssrsena_campusfes',
-            'ssrtemari_1st9another',
-            'ssrtemari_1st4another',
+            'ssrsaki_2nd',
+            'ssrsumika_1st',
+            'ssrhiro_3rd',
+            'ssrume_1st8another',                        
+            'ssrsaki_animate',
+            'ssrrinami_summerlimited',                        
         ],
         sssr: [],
         sr_card: []
