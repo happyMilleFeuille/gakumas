4c0f02f02606ad1ce8904f38a993d18a74006158
happyMilleFeuille <chyoai@gmail.com>
2026-04-26 16:19:14 +0900

    feat: add Misuzu 3rd SSR, update Hiro song title, and add card metadata

--- carddata.js
+++ carddata.js
@@ -2490,6 +2490,7 @@ export const cardList = [
         plan: "anomaly",
         source: "limited",
         have: "item",
+
         item_effects: [{ type: "action", trigger: ["get_preservation"], stats: { vocal: 30 }, target: "delete_t", max: 1 }],
         rarity: "SR",
         extra1: "param",
