590e7198d3b079f6b3768885b72820f39ef57dd3
happyMilleFeuille <chyoai@gmail.com>
2026-06-12 22:24:25 +0900

    video

--- videodata.js
+++ videodata.js
@@ -6,7 +6,7 @@ export const videoList = {
             title_ko: "생일 미니라이브2026",
             title_en: "Birthday MiniLive 2026",
             url: "https://youtu.be/26dpDbWQmLY?si=jDfmmIoYv9s2R6Y-",
-            date: "2026.04.01."
+            date: "2026.04.02."
         },
         {
             title: "EGO",
@@ -37,14 +37,14 @@ export const videoList = {
             title_ko: "생일 라이브방송2026",
             title_en: "Birthday stream 2026",
             url: "https://youtu.be/QJ9Qkypdbqs?si=771sO2TWBe-SvNOG",
-            date: "2026.06.02."
+            date: "2026.06.03."
         },
         {
             title: "生誕ミニライブ2025",
             title_ko: "생일 미니라이브2025",
             title_en: "Birthday MiniLive 2025",
             url: "https://youtu.be/G_Hf9yBdPfw?si=yW13nQSxGut3RpWy",
-            date: "2025.06.02."
+            date: "2025.06.03."
         },
         {
             title: "SUPREMACY",
@@ -72,7 +72,7 @@ export const videoList = {
             title_ko: "생일 미니라이브2026",
             title_en: "Birthday MiniLive 2026",
             url: "https://youtu.be/2Nj9mKhi1UA?si=8nf0jtyAtXqviF1h",
-            date: "2026.04.28"
+            date: "2026.04.29"
         },
         {
             title: "SUPREMACY",
@@ -121,7 +121,7 @@ export const videoList = {
             title_ko: "생일 미니라이브2026",
             title_en: "Birthday MiniLive 2026",
             url: "https://youtu.be/WYOyZYAGVpM?si=tyjWRSzZt5woUB7c",
-            date: "2026.01.17."
+            date: "2026.01.18."
         },
         {
             title: "ナイワ",
@@ -149,7 +149,7 @@ export const videoList = {
             title_ko: "생일 미니라이브2025",
             title_en: "Birthday MiniLive 2025",
             url: "https://youtu.be/97MA984Yzcc?si=ehD367nFFQFnISAY",
-            date: "2025.07.23."
+            date: "2025.07.24."
         },
         {
             title: "SUPREMACY",
@@ -170,7 +170,7 @@ export const videoList = {
             title_ko: "생일 미니라이브2025",
             title_en: "Birthday MiniLive 2025",
             url: "https://youtu.be/CD93hMXh6Yc?si=eyKP5R6z3SETvBoN",
-            date: "2025.07.31."
+            date: "2025.08.01."
         },
         {
             title: "ときめきのソルフェージュ  ",
@@ -198,7 +198,7 @@ export const videoList = {
             title_ko: "생일 미니라이브2025",
             title_en: "Birthday MiniLive 2025",
             url: "https://youtu.be/R_T8kAUTeZc?si=LdwKEwhQVgffFo_q",
-            date: "2025.11.10."
+            date: "2025.11.11."
         },
         {
             title: "SUPREMACY",
@@ -226,7 +226,7 @@ export const videoList = {
             title_ko: "생일 미니라이브2025",
             title_en: "Birthday MiniLive 2025",
             url: "https://youtu.be/iq7BCUXAaxA?si=3TmX5W7DI_bjLjVs",
-            date: "2025.12.20."
+            date: "2025.12.21."
         },
         {
             title: "Let's GO!! ICHI-NO-NI!!",
@@ -247,7 +247,7 @@ export const videoList = {
             title_ko: "생일 미니라이브2025",
             title_en: "Birthday MiniLive 2025",
             url: "https://youtu.be/JN74QXOJQaA?si=zVRx_-U1eDBbVL3J",
-            date: "2025.12.06."
+            date: "2025.12.07."
         },
         {
             title: "わかし・さわがし・スカパンク",
@@ -284,7 +284,7 @@ export const videoList = {
             title_ko: "생일 미니라이브2026",
             title_en: "Birthday MiniLive 2026",
             url: "https://youtu.be/uXaWfs608p8?si=PuSSooJziUtSYpZx",
-            date: "2026.02.05."
+            date: "2026.02.06."
         },
         {
             title: "わかし・さわがし・スカパンク",
@@ -319,7 +319,7 @@ export const videoList = {
             title_ko: "생일 미니라이브2026",
             title_en: "Birthday MiniLive 2026",
             url: "https://youtu.be/r4wylrjOuq8?si=gEjoWPIsCEkNSQh3",
-            date: "2026.03.31."
+            date: "2026.04.01."
         },
         {
             title: "わかし・さわがし・スカパンク",
@@ -347,7 +347,7 @@ export const videoList = {
             title_ko: "생일 미니라이브2026",
             title_en: "Birthday MiniLive 2026",
             url: "https://youtu.be/yxL7dK1pr7g?si=QvdODxts4bYmBvvq",
-            date: "2026.03.04."
+            date: "2026.03.05."
         },
         {
             title: "ナイワ",
