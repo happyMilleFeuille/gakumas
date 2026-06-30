9379447dbe9ab6a8f4f866753267ac16b8f56b3e
happyMilleFeuille <chyoai@gmail.com>
2026-06-30 14:31:50 +0900

    4343434

--- firebase-sync.js
+++ firebase-sync.js
@@ -79,7 +79,7 @@ export function triggerCloudSave() {
         } catch (error) {
             console.error("클라우드 저장 에러:", error);
         }
-    }, 3000); // 3초 대기 후 전송 (트래픽 최적화)
+    }, 1000); // 1초 대기 후 전송 (트래픽 최적화)
 }
 
 // storage-override.js에서 디스패치된 이벤트를 수신하여 자동 백업 실행
